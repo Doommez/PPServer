870d036d396cb0cb7d624394a1c9f4f2b4fcbb85
E. Boganov <jenyaz862@gmail.com>
2025-11-04 17:45:47 +0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  })
   app.useGlobalPipes(new ValidationPipe())
 
   app.use(cookieParser())
